baec259a37a143631bc65d80816c07310c0d53b9
João Lenon <lenonSec7@gmail.com>
2022-04-08 01:03:51 -0300

    feat: update packages

--- tests/Stubs/routes/http.ts
+++ tests/Stubs/routes/http.ts
@@ -1,4 +1,4 @@
-import { Route } from 'src/Facades/HttpRoute'
+import { Route } from '@athenna/http'
 
 Route.get('/healthcheck', ({ response }) => {
   return response.status(200).send({ status: 'ok' })
