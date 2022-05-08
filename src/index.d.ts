10abae1260eae0c8577162a9c9664767b2b3d562
João Lenon <lenonSec7@gmail.com>
2022-05-08 11:21:47 -0300

    chore(types): add types for application and ignite

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,42 @@
-export class Bootstrap {
-    /**
-     * Executes the application.
-     *
-     * @param {string[]} args
-     * @return {string}
-     */
-    static main(...args: string[]): string;
+import { Logger } from '@athenna/logger'
+
+export class Ignite {
+  /**
+   * Mocking the Logger when client doesn't want to show it.
+   *
+   * @return {Logger}
+   */
+  static getLogger(): Logger
+
+  fire(): Promise<Application>
+
+  /**
+   * Get the instance of the application inside of Ignite class.
+   *
+   * @return {Application}
+   */
+  getApplication(): Application
+}
+
+export class Application {
+  /**
+   * Boot a new Artisan inside this Application instance.
+   *
+   * @return {Promise<import('@athenna/artisan').ArtisanImpl>}
+   */
+  bootArtisan(): Promise<import('@athenna/artisan').ArtisanImpl>
+
+  /**
+   * Boot a new HttpServer inside this Application instance.
+   *
+   * @return {Promise<import('@athenna/http').Http>}
+   */
+  bootHttpServer(): Promise<import('@athenna/http').Http>
+
+  /**
+   * Shutdown the HttpServer inside this Application instance.
+   *
+   * @return {Promise<void>}
+   */
+  shutdownHttpServer(): Promise<void>
 }
