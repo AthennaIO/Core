dc82bdca1edede9a7a68e3688acb5a35b945dd84
João Lenon <lenonSec7@gmail.com>
2023-02-13 15:15:50 -0300

    docs(types): add description to RC options

--- src/Types/RcOptions.ts
+++ src/Types/RcOptions.ts
@@ -8,11 +8,46 @@
  */
 
 export interface RcOptions {
+  /**
+   * The Athenna version that is running.
+   */
   version?: boolean
+
+  /**
+   * Set if Athenna is running as a TypeScript application or not.
+   */
   typescript?: boolean
+
+  /**
+   * Set if the Rc file is being used inside package.json file in the "athenna" property.
+   */
   isInPackageJson?: boolean
+
+  /**
+   * Set the path to all the services of your application.
+   */
+  services?: string[]
+
+  /**
+   * Set the preload files that Athenna needs to preload in the application bootstrap.
+   */
   preloads?: string[]
+
+  /**
+   * Set the providers that Athenna needs to register in the application bootstrap.
+   */
   providers?: string[]
+
+  /**
+   * Set the commands that Athenna needs to register when running an Artisan command.
+   */
   commands?: string[]
-  commandsManifest?: string[]
+
+  /**
+   * Set the commands manifest where the key is the signature of your command and the
+   * value is the path to your command. If the command that you are running is inside
+   * the commandsManifest property, Athenna will import ONLY that specific command,
+   * ignoring the entire "commands" array.
+   */
+  commandsManifest?: Record<string, string>
 }
